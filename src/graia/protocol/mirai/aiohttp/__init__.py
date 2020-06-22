9881e0d632c961d10df4158ea294468a8a76a2e9
Chenwe-i-lin <1846913566@qq.com>
2020-06-22 16:01:23 +0800

    prepared for other developers

--- src/graia/protocol/mirai/aiohttp/__init__.py
+++ src/graia/protocol/mirai/aiohttp/__init__.py
@@ -1,8 +1,11 @@
 from graia.protocol.core import GraiaProtocol, exceptions as ProtocolExceptions
 from .session import MiraiSession
+from aiohttp import ClientSession
 
 class MiraiAiohttpProtocol(GraiaProtocol):
     session: MiraiSession
+    http_session: ClientSession
 
     def __init__(self, session: MiraiSession):
         self.session = session
+        self.http_session = ClientSession()
